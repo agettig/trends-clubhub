fd00b019fd3c409a6ae3baecbcbe9b2bf08c2a09
Raymond Lin <rl569@cornell.edu>
2022-04-27 20:51:21 -0400

    welcome

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,14 +1,16 @@
-import {  Center, Container, Heading, Text } from "@chakra-ui/react"
+import {  Center, Circle, Container, Heading, Square, Text } from "@chakra-ui/react"
 import Layout from "../components/layout/Layout"
 
 const TutorialPage = () => {
     return (
         <div>
             <Layout title="Tutorial">
-                <Heading>Welcome!</Heading>
-                <Text>Navigate through the website using the navigation bar on the top.</Text>
-                <Text>Create an account by signing up with Google.</Text>
-                <Text>Post an event by ...</Text>
+                <Center>
+                    <Heading my="4">Welcome!</Heading>
+                </Center>
+                <Circle my="4">Navigate through the website using the navigation bar on the top.</Circle>
+                <Center my="4">Create an account by signing up with Google.</Center>
+                <Center my="4">Post an event by ...</Center>
             </Layout>
         </div>
     )
