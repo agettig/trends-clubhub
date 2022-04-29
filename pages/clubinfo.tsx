f593284ba7aeb52a923f36db7367e6e2e7403603
Ariela Gettig <aqg3@cornell.edu>
2022-04-29 10:14:22 -0400

    add clubinfo to nav, change width, external links

--- pages/clubinfo.tsx
+++ pages/clubinfo.tsx
@@ -2,10 +2,9 @@ import ClubInfo from "../components/club/ClubInfo";
 import Layout from "../components/layout/Layout"
 
 const ClubInfoPage = () => (
-    <Layout title="Club Info">
-      <ClubInfo />
-    </Layout>
-  )
-  
-  export default ClubInfoPage
-  
+  <Layout title="Club Info">
+    <ClubInfo />
+  </Layout>
+)
+
+export default ClubInfoPage
