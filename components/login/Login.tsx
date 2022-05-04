d35f83c989d6c002d1f425e57f0c5ee6141c587e
harrisonchin2523 <harrisonchin2523@gmail.com>
2022-05-04 17:57:45 -0400

    finished oauth-login

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -1,50 +1,24 @@
-// import { addDoc, collection, query } from "firebase/firestore";
-import { Button, Input } from "@chakra-ui/react";
+import { addDoc, collection, query } from "firebase/firestore";
+import GoogleButton from "react-google-button";
+import { FacebookLoginButton } from "react-social-login-buttons";
 import { MouseEventHandler, useState } from "react";
 import styles from "./login.module.css";
-// import { db } from "../../util/firebase"
+import { db, signInWithGoogle } from "../../util/firebase";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
   const login: MouseEventHandler<HTMLButtonElement> | undefined = (e) => {
     e.preventDefault();
-
-    setUsername("");
-    setPassword("");
   };
 
   return (
     <div className={styles.page}>
       <div className={styles.form}>
         <div className={styles.title}>Sign In</div>
-        <div className={styles.text}>
-          <Input
-            value={username}
-            type="text"
-            placeholder="John Doe"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className={styles.password}>
-          <Input
-            value={password}
-            type="password"
-            placeholder="QWERTY1234"
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <div className={styles.button}>
+          <GoogleButton onClick={signInWithGoogle} />
         </div>
         <div className={styles.button}>
-          <Button
-            type="submit"
-            onClick={login}
-            _focusVisible={{ shadow: "outline" }}
-            _focus={{ shadow: "none" }}
-            colorScheme={"facebook"}
-          >
-            Login
-          </Button>
+          <FacebookLoginButton onClick={signInWithGoogle} />
         </div>
       </div>
     </div>
---
+++ util/firebase.ts
@@ -0,0 +1,50 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import withFirebaseAuth from "react-with-firebase-auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signOut,
+  signInWithPopup,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXOhHJOGTEiqiSjS4YrQ4KHr9UX8gd1QQ",
+  authDomain: "trends-clubhub.firebaseapp.com",
+  projectId: "trends-clubhub",
+  storageBucket: "trends-clubhub.appspot.com",
+  messagingSenderId: "944154353210",
+  appId: "1:944154353210:web:ab64536c28a075f34a5f03",
+  measurementId: "G-DH7LPRTD1L",
+};
+
+const providers = {
+  googleProvider: new GoogleAuthProvider(),
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+const createComponentWithAuth = withFirebaseAuth({
+  providers,
+  firebaseAppAuth: auth,
+});
+
+const signInWithGoogle = () => {
+  signInWithPopup(auth, providers.googleProvider);
+};
+
+const signOutFirebase = () => {
+  signOut(auth);
+};
+
+export {
+  db,
+  auth,
+  createComponentWithAuth,
+  signInWithGoogle,
+  signOutFirebase as signOut,
+};
